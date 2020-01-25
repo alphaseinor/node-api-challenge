fc50af2ec7b17352e70d0b293e0c1c9152177c7f
Brian Hague <alphaseinor@gmail.com>
2020-01-24 20:41:48 -0600

    added get for api projects

--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -1,10 +1,19 @@
 const express = require('express')
-const projectDb = require('../data/dbConfig')
+const projectDb = require('../data/helpers/projectModel')
 
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.send(`Project Endpoint`)
+  //res.send(`Project Endpoint`)
+  projectDb.get()
+    .then(users => {
+      res.status(200).json(users)
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: "The posts information could not be retrieved."
+      })
+    })  
 })
 
 router.get('/:id', (req, res) => {
